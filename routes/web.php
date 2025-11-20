277f530f7b53762c105adc019653cfe53bdd0366
maulhihi <maulhihi@gmail.com>
2025-11-20 14:49:30 +0700

    Add login & register with Laravel Breeze and custom template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'admin']);
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Admin
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'admin'])->name('dashboard');
+
+// // User
+// Route::get('/', function () {
+//     return view('home');
+// })->middleware(['auth'])->name('home');
+
+Route::resource('/products', ProductController::class)->middleware(['auth']);
+
+Route::resource('/category', CategoryController::class);
+
+require __DIR__.'/auth.php';
